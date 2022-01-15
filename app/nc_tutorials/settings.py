09886f98968862fbbd19224d5c108f43f68aa6d3
Becky Joplin <bjoplin@overstock.com>
2022-01-15 11:04:10 -0700

    second commit

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 else:
-    STATIC_ROOT = [os.path.join(BASE_DIR, "statis")]
+    STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 
 STATIC_URL = '/static/'
 
